8ebd6cf0bb5467ced4abc50cb70f1d23273f21e6
NileshVirkar148 <75842704+NileshVirkar148@users.noreply.github.com>
2022-06-03 13:48:21 +0530

    Add files via upload

---
+++ src/main/java/demomaven/Copy_DemoMaven.java
@@ -0,0 +1,27 @@
+package demomaven;
+
+public class Copy_DemoMaven {
+
+	public static void main(String[] args) {
+		int k;
+	}
+	
+	public boolean unusedMethod(boolean k) {
+		k = true;
+		
+        String foo = null;
+        System.out.println(foo.length());
+        
+		return k;
+	}
+	
+	public boolean unusedMethod2(boolean k) {
+		k = true;
+		
+        String foo = null;
+        System.out.println(foo.length());
+        
+		return k;
+	}
+
+}
---
+++ src/main/java/demomaven/DemoMaven.java
@@ -0,0 +1,21 @@
+package demomaven;
+
+public class DemoMaven {
+
+	public static void main(String[] args) {
+		int k;
+	}
+	
+	public boolean unusedMethod(boolean k) {
+		k = true;
+		
+        
+		return k;
+	}
+	
+	public boolean unusedMethod2(boolean k) {
+		boolean p=true;
+		return p;
+	}
+
+}
